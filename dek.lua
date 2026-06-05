36906a323a8983c00ef84f9e52d9fee3553a7a5a
L-Lawliet-Hub <hchandra4358@gmail.com>
2026-06-05 20:28:10 +0530

    Update dek.lua

--- dek.lua
+++ dek.lua
@@ -1326,7 +1326,7 @@ local function setupAutoExecute()
 			repeat task.wait() until game:IsLoaded()
 			task.wait(5)
 			getgenv().AutoExec = false
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/L-Lawliet-Hub/THUB/main/ins.lua"))()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/L-Lawliet-Hub/THUB/main/dek.lua"))()
 		]])
 	end
 end
