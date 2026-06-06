c972d7ef21196dc3abb679c4764f8c6ba1909bf8
L-Lawliet-Hub <hchandra4358@gmail.com>
2026-06-06 13:14:03 +0530

    Update dek.lua

--- dek.lua
+++ dek.lua
@@ -1082,8 +1082,6 @@ if rewards then
 								"User: " .. lp.Name .. "\n" ..
 								"Games Played: " .. tostring(gamesPlayed) .. "\n" ..
 								"Executor: " .. executor .. "\n" ..
-								"Blacklisted: " .. (bl and "YES ❌" or "No ✅") ..
-				              "\nExploiter: " .. (ex and "YES ❌" or "No ✅") ..
 								"\n```",
 							inline = true
 						},
