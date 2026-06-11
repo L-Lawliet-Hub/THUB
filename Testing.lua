72c929d33377ebe4d843427e4a867017ba5d2091
L-Lawliet-Hub <hchandra4358@gmail.com>
2026-06-11 17:37:22 +0530

    Update Testing.lua

--- Testing.lua
+++ Testing.lua
@@ -1316,7 +1316,7 @@ local function setupAutoExecute()
 			repeat task.wait() until game:IsLoaded()
 			task.wait(5)
 			getgenv().AutoExec = false
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/L-Lawliet-Hub/THUB/main/ins.lua"))()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/L-Lawliet-Hub/THUB/main/Testing.lua"))()
 		]])
 	end
 end
